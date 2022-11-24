3782b4cc1467ddcdd0afc3c7b6f3214af215ab6b
JaehyeonCho93 <jh.cho142857@gmail.com>
2022-11-24 19:02:41 +0900

    2022. 11. 24 day3

---
+++ Day3/짝수는싫어요.js
@@ -0,0 +1,16 @@
+/*
+문제 설명
+정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+//내 풀이
+
+function solution(number) {
+  let result = [];
+  for (let i = 1; i <= number; i += 2) {
+    result.push(i);
+  }
+  return result;
+}
+
+console.log(solution(11));
---
+++ Day3/최빈값구하기.js
@@ -0,0 +1,11 @@
+/*
+문제 설명
+최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
+*/
+
+
+//Map 쓰는거
+https://velog.io/@jayjay8/JS-sort%EB%A1%9C-%EC%9D%B4%EC%A4%91%EB%B0%B0%EC%97%B4-%EC%A0%95%EB%A0%AC%ED%95%98%EA%B8%B0
+
+//Map 안쓰는거
+https://leejams.github.io/%EC%B5%9C%EB%B9%88%EA%B0%92-%EA%B5%AC%ED%95%98%EA%B8%B0/
